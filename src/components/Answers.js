da92cf70ac9e567d460c8e14684f0e829b4296eb
c-annie <69065815+c-annie@users.noreply.github.com>
2021-10-29 01:08:37 -0400

    fixed merged stuffs

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,131 +1,44 @@
 import React, { useState, useEffect, forwardRef, useRef, useImperativeHandle } from 'react'
 import { makeStyles, styled } from '@material-ui/core/styles';
-import { Box, Button, FormControl, InputBase, TextField, InputAdornment, IconButton} from '@mui/material'
+import { Box, Button, FormControl, InputBase, TextField } from '@mui/material'
 import { useHistory, useLocation } from 'react-router-dom';
-import DeleteIcon from '@mui/icons-material/HighlightOff';
 
 const useStyles = makeStyles((theme) => ({
   answerWrapper: {
-<<<<<<< Updated upstream
-    paddingTop: 10, 
-    paddingLeft: 50,
-    paddingRight: 31,
-=======
     display: 'inline-block',
     paddingLeft: 50,
     paddingRight: 31,
     paddingBottom: 10,
     marginTop: 10, 
-    marginLeft: 100,
->>>>>>> Stashed changes
+    marginLeft: 80,
     fontSize: 22,
-    //fullWidth: 30,
-    borderRadius: '70px',
   },
   answerText: {
     textAlign: 'left',  
     marginTop: 10,
-<<<<<<< Updated upstream
-    marginLeft: 30,
-    width: '45vw'
-
-=======
     width: '38vw',
     backgroundColor: "#FFFFFF",
->>>>>>> Stashed changes
   },
-//   toolbar: theme.mixins.toolbar,
 }));
 
 function Answers(props, ref) {
   const classes = useStyles();
-
-  const [answers, setAnswers] = useState([]);
-  const [answer, setAnswerText] = useState({
-    answer_text: '',
-  })
-
-  useImperativeHandle( ref, () => ({
-    getanswers() {
-      return answers;
-    }
-  }));
-  
-    //cretes new textbox
-  const addAnswer = (e) => {
-    let currentanswers = [...answers]
-    let newanswer = "New answer";
-    currentanswers.push(newanswer);
-    setAnswers(currentanswers);
-  }
-
-  const removeAnswer = index => e => {
-    let currentanswers = [...answers]
-    currentanswers.splice(index,1)
-    setAnswers(currentanswers);
-  }
-
-    //updates list of answers
-    const onAnswerTextChange = index => e => {
-      let tempAnswers = [...answers];
-      setAnswerText({...answer.answer_text, answer_text:e.target.value});
-      tempAnswers[index] = e.target.value;
-      setAnswers(tempAnswers);
-    }
-
-    const deleteStyle = {
-      backgroundColor: '#8A8AEE',
-      marginLeft: 10,
-      marginBottom: 10,
-      color: 'black',
-      float: "right",
-      borderRadius: 20
-    }
-
-    const addStyle = {
-      backgroundColor: '#8A8AEE',
-      marginLeft: 10,
-      marginTop: 10,
-      marginBottom: 10,
-      marginRight: 10,
-      color: 'black',
-      float: "right",
-      borderRadius: 20
-    }
-
       
-    return(
-      <Box className={classes.preset} disableElevation>
-        <div className={classes.toolbar} />
-        {answers.map((answer, index) =>{
-          return (
-          <Box className={classes.answerWrapper} key={index} disableElevation> 
-            <TextField className={classes.answerText}
-                key={index}
-                value={answers[index]}
-                onChange={onAnswerTextChange(index)}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end" variant='contained'>
-                      <IconButton onClick={removeAnswer(index)}>
-                         <DeleteIcon disableElevation/>
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-                inputProps={{
-                style: {
-                    padding: 8,
-                    fontSize: 20, 
-                }, 
-            }}
-          />
-          </Box>
-          );
-        })}
-        <Button style={addStyle} variant='contained' onClick={addAnswer} disableElevation >+ Add answer</Button>
-      </Box>
-    );
+  return(
+    <Box className={classes.answerWrapper} key={props.id}> 
+      <TextField className={classes.answerText}
+          key={props.id}
+          value={props.answerText}
+          onChange={e => props.callback(props.id)}
+          inputProps={{
+          style: {
+              padding: 5,
+              fontSize: 20,
+          }
+        }}
+      />
+    </Box>
+  );
 }
 
 export default forwardRef(Answers);
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect, forwardRef, useRef, useImperativeHandle } from 'react'
-import { makeStyles, styled } from '@material-ui/core/styles';
-import { Box, Button, FormControl, InputBase, TextField } from '@mui/material'
-import { useHistory, useLocation } from 'react-router-dom';
+import React, { forwardRef } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, TextField } from '@mui/material'
 import Answers from "./Answers"
-import { InputAdornment } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -12,13 +10,8 @@ const useStyles = makeStyles((theme) => ({
   questionWrapper: {
     display: 'inline-block',
     fontSize: 22,
-<<<<<<< Updated upstream
-    paddingBottom: 10,
-    paddingLeft: 70,
-=======
     marginLeft: 70,
     marginTop: 20
->>>>>>> Stashed changes
   },
   questionText: {
     textAlign: 'left', 
@@ -28,94 +21,25 @@ const useStyles = makeStyles((theme) => ({
   },
   //toolbar: theme.mixins.toolbar,
 }));
-
 function Questions(props, ref) {
   const classes = useStyles();
-  const answersRef = useRef();
 
-  const [questions, setQuestions] = useState([]);
-  const [question, setQuestionText] = useState({
-    question_text: '',
-  })
-
-  useImperativeHandle( ref, () => ({
-    getQuestions() {
-      return questions;
-    }
-  }));
-  
-    //cretes new textbox
-  const addQuestion = (e) => {
-    let currentquestions = [...questions]
-    let newquestion = "New question";
-    currentquestions.push(newquestion);
-    setQuestions(currentquestions);
-  }
-
-  const removeQuestion = index => e => {
-    let currentquestions = [...questions]
-    currentquestions.splice(index,1)
-    setQuestions(currentquestions);
-  }
-
-    //updates list of questions
-    const onQuestionTextChange = index => e => {
-      let tempQuestions = [...questions];
-      setQuestionText({...question.question_text, question_text:e.target.value});
-      tempQuestions[index] = e.target.value;
-      setQuestions(tempQuestions);
-    }
-
-    const deleteStyle = {
-      backgroundColor: '#8A8AEE',
-      marginTop: 10,
-      marginBottom: 10,
-      color: 'black',
-      borderRadius: 50
-    }
-
-    const addStyle = {
-      backgroundColor: '#8A8AEE',
-      left: "8%",
-      marginBottom: 10,
-      color: 'black',
-      width: "50vw",
-      borderRadius: 20
-    }
-      
-    return(
-      <Box className={classes.box}>
-      <div className={classes.toolbar} />
-      { questions.map((question, index) =>{
-          return (
-          <Box className={classes.questionWrapper} key={index}> 
-          {index+1}. <TextField className={classes.questionText}
-            key={index}
-            value={questions[index]}
-            onChange={onQuestionTextChange(index)}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end" variant='contained'>
-                   <Button style={deleteStyle} variant='contained' onClick={removeQuestion(index)}>X</Button>
-                </InputAdornment>
-              )
-            }}
-            inputProps={{
-              style: {
-                padding: 5,
-                fontSize: 20,
-              }
-            }}
-          />
-            <Answers ref={props, answersRef}/>  
-            <div className={classes.toolbar} />   
-          </Box>
-          );
-        })}
-        <div className={classes.toolbar} />
-        <Button style={addStyle} variant='contained' onClick={addQuestion} >+ Add question</Button>
-      </Box>
-    );
+  return(
+    <Box className={classes.questionWrapper} key={props.id}> 
+      {props.id + 1}. 
+      <TextField className={classes.questionText}
+        key={props.id}
+        value={props.questiontext}
+        onChange={props.callback(props.id)}
+        inputProps={{
+          style: {
+            padding: 5,
+            fontSize: 20,
+          }
+        }}
+      />
+    </Box> 
+  );
 }
 
 export default forwardRef(Questions);
