133225546059b3c3527bdc1ec7e6538473ddf756
Michelle Tham <michelle.tham@stonybrook.edu>
2021-11-08 17:38:55 -0500

    Popup dialog for edit username

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Button, Box, Grid, Typography, ImageListItem } from '@material-ui/core'
+import { Button, Box, Grid, Typography, ImageListItem, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
 import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import { ThemeProvider } from '@material-ui/styles';
 import * as constants from '../components/constants';
 
@@ -57,17 +62,52 @@ export default function Profile(props) {
     points: [],
   });
   const classes = useStyles();
-  useEffect(() => {
-    axios.get(`${constants.API_PATH}/users/${props.match.params.user_id}`)
-      .catch(err => {
-        console.log('Profile: ', err);
-      }).then(res => {
-        console.log(res)
-        if (res.status == 200) {
-          setState({ user: { ...res.data.user }, points: [...res.data.points] });
-        }
-      })
-  }, [props]);
+  const [open, setOpen] = React.useState(false);
+
+  const copyState = () => {
+    let new_user = state.user;
+    let new_points = [...state.points];
+    return [new_user, new_points];
+  }
+
+  const onUsernameChange = (e) => {
+    let [new_user, new_points] = copyState();
+    new_user.username = e.target.value;
+    setState({
+      user: new_user,
+      points: new_points,
+    });
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleSaveEditName = () => {
+    axios.put(`${constants.API_PATH}/users/${props.match.params.user_id}`, {
+      user_fields: {username: state.user.username}
+    }).then(res => {
+      // TODO: afterupdate
+    }).catch(err => {
+      console.log('PUT on Save: ', err);
+    })
+
+    setOpen(false);
+  };
+
+  // useEffect(() => {
+  //   axios.get(`${constants.API_PATH}/users/${props.match.params.user_id}`)
+  //     .catch(err => {
+  //       console.log('Profile: ', err);
+  //     }).then(res => {
+  //       console.log(res)
+  //       if (res.status == 200) {
+  //         setState({ user: { ...res.data.user }, points: [...res.data.points] });
+  //       }
+  //     })
+  // }, [props]);
 
   return (
     <Box className={classes.profilePage}>
@@ -75,6 +115,17 @@ export default function Profile(props) {
       <img className={classes.icon} src={state.user && state.user.picture} />
       <Box className={classes.username}>
         <Typography>{state.user && state.user.username}</Typography>
+        <Button variant="outlined" onClick={handleClickOpen}>Edit Username</Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Edit Username</DialogTitle>
+          <DialogContent>
+            <TextField autoFocus margin="dense" id="name" label="Username" type="name" fullWidth variant="standard"/> 
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={onUsernameChange, handleSaveEditName}>Save</Button>
+          </DialogActions>
+        </Dialog>
       </Box>
       <Box className={classes.totalPoints}>
         <Typography>Total Points: {state.user && state.user.points}</Typography>
