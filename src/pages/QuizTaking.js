da2c8088784c3a9cb1d82b0bd6eeb86d34332161
Annie <anniechen62000@gmail.com>
2021-12-04 02:05:17 -0500

    quiz taking, creating, and result scaling

--- src/pages/QuizTaking.js
+++ src/pages/QuizTaking.js
@@ -12,7 +12,10 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     justifyContent: "flex-start",
-    width: "60vw",
+    width: theme.spacing(120),
+  },
+  box:{ 
+    backgroundColor: "#F9F9FF",
   },
   Opt: {
     display: "inline-block",
@@ -58,12 +61,15 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 15,
     borderTopLeftRadius: 15,
   },
+  questions: {
+    //display: "flex",
+    width: "100%",
+  },
   noBorder: {
     border: "none",
   },
   quizbody: {
     display: "flex",
-    backgroundColor: "#FFFFFF",
   },
   answerWrapper: {
     display: "inline-block",
@@ -81,7 +87,7 @@ const submitStyle = {
   left: "8%",
   marginBottom: 10,
   color: "black",
-  width: "50vw",
+  width: "80%",
   borderRadius: 20,
   marginTop: 10,
 };
@@ -297,7 +303,7 @@ export default function QuizTake(props) {
           <div className={classes.quizbody} />
           {state.questions &&
             state.questions.map((question, q_key) => (
-              <div key={q_key}>
+              <div className={classes.questions} key={q_key}>
                 <Questions q_key={q_key} q_text={question} readOnly />
                 <Grid direction="row" container>
                   <Grid direction="column" container item sm={6}>
@@ -325,6 +331,7 @@ export default function QuizTake(props) {
                 </Grid>
               </div>
             ))}
+            <div className={classes.quizbody} >
           <Button
             style={submitStyle}
             variant="contained"
@@ -334,6 +341,7 @@ export default function QuizTake(props) {
           >
             Submit
           </Button>
+          </div>
         </Box>
       </FormControl>
     </Box>
