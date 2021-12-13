fc41a26e7d5e1638af269d9194f5494407ab9213
POPSICKLES <popsickles.py@gmail.com>
2021-12-13 15:36:53 -0500

    fixed edit question bug

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -32,52 +32,120 @@ const useStyles = makeStyles((theme) => ({
   },
   //toolbar: theme.mixins.toolbar,
 }));
+const IncorrectTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#ce9093',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#ce9093',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#ce9093',
+    },
+    '&:hover fieldset': {
+      borderColor: '#ce9093',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#ce9093',
+    },
+  },
+});
+
+const CorrectTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: "#7cd370",
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: "#7cd370",
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: "#7cd370",
+    },
+    '&:hover fieldset': {
+      borderColor: "#7cd370",
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: "#7cd370",
+    },
+  },
+});
+
+//const EditTextField = styled(TextField)({
+//  '& label.Mui-focused': {
+//    color: c,
+//  },
+//  '& .MuiInput-underline:after': {
+//    borderBottomColor: c,
+//  },
+//  '& .MuiOutlinedInput-root': {
+//    '& fieldset': {
+//      borderColor: c,
+//    },
+//    '&:hover fieldset': {
+//      borderColor: c,
+//    },
+//    '&.Mui-focused fieldset': {
+//      borderColor: c,
+//    },
+//  },
+//});
 
 export default function Questions(props) {
   const classes = useStyles();
 
-  let c = "";
+  let c;
   if (props.variant === "show-correct")
-    c = "#7cd370";
-  else if (props.variant === "show-incorrect")
-    c = '#ce9093';
+    c = (<CorrectTextField
+      className={classes.questionText}
+      key={props.q_key}
+      value={props.q_text}
+      onChange={(e) => props.q_callback(e, props.q_key)}
+      inputProps={{
+        readOnly: props.readOnly,
+        style: {
+          padding: 5,
+          fontSize: 16,
+        },
+      }}
+    />)
+  else if (props.variant === "show-incorrect") {
+    c = (<IncorrectTextField
+      className={classes.questionText}
+      key={props.q_key}
+      value={props.q_text}
+      onChange={(e) => props.q_callback(e, props.q_key)}
+      inputProps={{
+        readOnly: props.readOnly,
+        style: {
+          padding: 5,
+          fontSize: 16,
+        },
+      }}
+    />)
+  }
+  else {
+    c = (<TextField
+      className={classes.questionText}
+      key={props.q_key}
+      value={props.q_text}
+      onChange={(e) => props.q_callback(e, props.q_key)}
+      inputProps={{
+        readOnly: props.readOnly,
+        style: {
+          padding: 5,
+          fontSize: 16,
+        },
+      }}
+    />)
+  }
 
-  const CssTextField = styled(TextField)({
-    '& label.Mui-focused': {
-      color: c,
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: c,
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: c,
-      },
-      '&:hover fieldset': {
-        borderColor: c,
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: c,
-      },
-    },
-  });
   return (
     <>
       <Box className={classes.questionWrapper} key={props.q_key}>
         {props.q_key + 1}.{" "}
-        <CssTextField
-          className={classes.questionText}
-          key={props.q_key}
-          value={props.q_text}
-          onChange={(e) => props.q_callback(e, props.q_key)}
-          inputProps={{
-            readOnly: props.readOnly,
-            style: {
-              padding: 5,
-              fontSize: 16,
-            },
-          }}
-        />
+        {c}
       </Box>
     </>
   );
